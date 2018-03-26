3a95c98b4d582e4f9918b07eb2f5f0708fcd93ff
KatarzynaZasada <katarzynaz439@gmail.com>
2018-03-26 21:00:41 +0200

    First commit

---
+++ assets/js/todos.js
@@ -0,0 +1,31 @@
+//dodawanie do listy
+
+  $("input").on("keypress", function(event){
+    if(event.which === 13) {
+    var newTodo = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + newTodo + "</li>");
+  };
+  });
+
+//przekreślanie toDo, mozna tez uzyc toggleClass
+ //$("li").click(function(){
+ //$(this).toggleClass("lineThrough");
+//});
+
+  $("ul").on("click", "li", function(){
+      if($(this).hasClass("lineThrough")) {
+        $(this).removeClass("lineThrough");
+      } else {
+        $(this).addClass("lineThrough");
+      }
+  });
+
+//usuwanie
+  $("ul").on("click", "i", function(event){
+    $(this).closest("li").remove();
+  });
+
+  $(".fa-plus").on("click", function() {
+    $("input").fadeToggle();
+  });
